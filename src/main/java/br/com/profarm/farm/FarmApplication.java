1d1e9d23bf870156870be0c8f2fbfc8bbd1b1ab3
Dyeimys Franco <dyeimys@outlook.com>
2022-04-09 11:33:32 -0300

    first commit

---
+++ src/main/java/br/com/profarm/farm/FarmApplication.java
@@ -0,0 +1,15 @@
+package br.com.profarm.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class FarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmApplication.class, args);
+    }
+
+}
